4b61966533ec0496ab3a4a0da9934dffa481d968
frutabruta <adameichr1@gmail.com>
2021-04-05 02:46:46 +0200

    first commit

---
+++ pasmo.cpp
@@ -0,0 +1,9 @@
+#include "pasmo.h"
+
+Pasmo::Pasmo()
+{
+
+
+}
+
+
---
+++ pasmo.h
@@ -0,0 +1,14 @@
+#ifndef PASMO_H
+#define PASMO_H
+#include <QObject>
+
+
+class Pasmo
+{
+public:
+    Pasmo();
+    QString nazev="";
+    QString system="";
+};
+
+#endif // PASMO_H
