d8f4f2e0120adc79285ed8bc492f342089c6a154
frutabruta <adameichr1@gmail.com>
2021-08-26 15:50:06 +0200

    přidání: routeDeviation="onroute"; vehicleSubMode="BusSubmode"; vehicleMode="regionalBus"; razziaState="norazzia";

--- cestaudaje.h
+++ cestaudaje.h
@@ -11,7 +11,11 @@ class CestaUdaje
 public:
     explicit CestaUdaje(QWidget *parent = nullptr);
     QString doorState="AllDoorsClosed";
-    QString locationState="AtStop";  
+    QString locationState="AtStop";
+    QString routeDeviation="onroute";
+    QString vehicleSubMode="BusSubmode";
+    QString vehicleMode="regionalBus";
+    QString razziaState="norazzia";
     bool VehicleStopRequested=false;
     bool prestupy=false;
     int indexAktZastavky;
